0f4178266c3413d44d7d33f33eec24b61404b698
Gulere Noah <123893161+Noahgulere@users.noreply.github.com>
2025-10-03 20:28:28 +0300

    Resume & portifo deletion

--- download.php
+++ download.php
@@ -4,7 +4,7 @@ $file = $_GET['file'];
 $file_path = "assets/resume.pdf".$file; // Change this to the path where your asset is stored
 
 if(file_exists($assets/resume.pdf)) {
-    header("Content-Disposition: attachment; filename=".$assets/resume.pdf);
+    header("Content-Disposition: attachment; filename=".$assets/);
     readfile($assets/resume.pdf);
     exit;
 } else {
